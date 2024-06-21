8150f80bf5c0dd79fc50b62915c9e1f56b1d5b2a
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-06-21 23:10:53 +1000

    Build the Listing Model and the Controllers

---
+++ models/recipe.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  ingredients: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Ingredient",
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema);
